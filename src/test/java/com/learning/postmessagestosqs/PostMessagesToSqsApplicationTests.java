02c8cf382c82e7f1360f7a3ef0c1e7f31c7b4711
shashank <shashankbillagiri93@gmail.com>
2023-10-15 15:43:38 -0500

    Created New Repository

---
+++ src/test/java/com/learning/postmessagestosqs/PostMessagesToSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.postmessagestosqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMessagesToSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
